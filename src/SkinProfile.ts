ac8eee696997837f6c5cb046e2e8b6a6592f1cd9
Tyler <tyler.holden@nextgencode.io>
2021-04-17 10:31:50 -0500

    remove duplicated data live value

--- src/SkinProfile.ts
+++ src/SkinProfile.ts
@@ -5,7 +5,6 @@ import { firestore } from 'firebase';
 
 export type SkinProfile = {
   version: string;
-  live: boolean;
   shoppingCart: DatabaseShoppingCart;
   regimen: SkincareRegimen;
   shoppingCartConfirmed: boolean;

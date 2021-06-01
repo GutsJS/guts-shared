21ef3dcba825871699f11308095ec3fea11e5f3f
Tyler <tyler.holden@nextgencode.io>
2021-06-01 15:09:44 -0500

    remove invoice finalization step in product purchasing

--- src/NotificationSystem.ts
+++ src/NotificationSystem.ts
@@ -26,7 +26,6 @@ export const NotificationEvents = [
   'skin-profile-publish-reminder-4-day',
   'review-created',
   'client-confirmed-product-selection',
-  'invoice-finalized',
   'client-invoice-charged',
 ] as const;
 export type NotificationEvents = typeof NotificationEvents[number];

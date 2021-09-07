362e58c5aa7c903cdc3b0533ed51f174136bc9b5
Tyler <tyler.holden@nextgencode.io>
2021-09-07 14:13:22 -0500

    add guid to hubspot forms

--- src/HubspotAPI.ts
+++ src/HubspotAPI.ts
@@ -52,27 +52,33 @@ export type HubspotFormField = {
 export type HubspotFormGroup = {
   id: string;
   name: string;
+  guid: string;
 };
 
 export const HubspotFormGroups: HubspotFormGroup[] = [
   {
     id: 'client_skin_data_-_pre-consultation_questionnaire',
     name: 'Pre Consultation Questionnaire',
+    guid: '3aaf9797-b29d-42e0-89bc-a53c5ab4a76c',
   },
   {
     id: 'client_skin_data_-_follow-ups',
     name: 'Follow-Ups',
+    guid: '50edee3f-5297-45fe-b0ae-35ccf6953b53',
   },
   {
     id: 'client_skin_date_-_follow-up_pre_consultation_questionnaire',
     name: 'Follow-Up Pre Consultation Questionnaire',
+    guid: '355147ae-5bf7-48b8-8d80-55b100a345cc',
   },
   {
     id: '6_week_after_consultation_-_skin_update_form',
     name: '6 Week After Consultation - Skin Update Form',
+    guid: 'ec000d5c-0c89-4bf1-a8bb-05319ceedb16',
   },
   {
     id: 'consultation_1_-_client_information',
     name: 'Initial Consultation',
+    guid: '2f4c756b-ad83-4d9f-a08c-6fd0f8adb336',
   },
 ];

466ce1f9c51f66c4f0f5af043b887407bb5c0310
Tyler <tyler.holden@nextgencode.io>
2021-09-23 15:30:07 -0500

    feat: add api requests table

--- src/types/API.ts
+++ src/types/API.ts
@@ -1,7 +1,16 @@
 import { SiteData } from '..';
 
+export type APIError = {
+  message: string;
+};
+
 export type SaveSiteDataRequest = {
   token: string;
+  siteData: SiteData;
+};
+
+export type SaveSiteDataResponse = {
+  success: boolean;
   siteData: SiteData;
 };
 
@@ -9,3 +18,23 @@ export type GetSiteDataRequest = {
   token: string;
   siteId: string;
 };
+
+export type GetSiteDataResponse = {
+  success: boolean;
+  siteData: SiteData;
+};
+
+export type PublicRequests = 'saveSiteData' | 'getSiteData';
+
+export type APIRequests<RequestName = PublicRequests> =
+  RequestName extends 'saveSiteData'
+    ? {
+        request: SaveSiteDataRequest;
+        response: APIError | SaveSiteDataResponse;
+      }
+    : RequestName extends 'getSiteData'
+    ? {
+        request: GetSiteDataRequest;
+        response: APIError | GetSiteDataResponse;
+      }
+    : never;

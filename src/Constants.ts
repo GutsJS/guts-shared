1ca0cd7a0dbb3c1437eb1752b9daeb7b98df516d
Tyler <tyler.holden@nextgencode.io>
2021-09-23 12:00:40 -0500

    init

--- src/Constants.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_CONSULTANT_INTRO_CHAT_MESSAGE =
-  "Hi, I'm your new skin mentor. Let me know if you have any questions!";
--- src/utilFunctions/priceToString.ts
+++
@@ -1,9 +0,0 @@
-import { Currencies, CurrencyToSymbol } from '../GlobalTypes';
-
-export const priceToString = (
-  price: number | undefined,
-  currency: Currencies | undefined
-) => {
-  if (!price || !currency) return '--';
-  return CurrencyToSymbol[currency] + String(price.toFixed(2));
-};

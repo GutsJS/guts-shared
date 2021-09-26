72f0f8da370e161fa7c301163643a3a5734827c5
Tyler <tyler.holden@nextgencode.io>
2021-09-26 12:18:16 -0500

    better obj checking

--- src/utils/checkSiteDataPlanLimits.ts
+++ src/utils/checkSiteDataPlanLimits.ts
@@ -1,21 +1,40 @@
 import { GutsElement, PLAN_LIMITS, SiteData } from '..';
 
+/**
+ * @param siteData
+ * @returns boolean whether there are more elements than allowed;
+ * False = under limit;
+ * True = OVER limit;
+ */
 export const checkElementLimit = (siteData: SiteData): boolean => {
+  if (!siteData?.elements) return false;
   return (
     Object.keys(siteData.elements).length >
     PLAN_LIMITS[siteData.plan].totalElements
   );
 };
 
+/**
+ *
+ * @param siteData
+ * @returns false for under limit OR the elements of which have too many routes
+ */
 export const checkAllElementsRouteLimit = (
   siteData: SiteData
 ): false | GutsElement[] => {
+  if (!siteData?.elements) return false;
   const elementsWithIssue = Object.keys(siteData.elements)
     .map((el) => checkElementRouteLimit(siteData.elements[el], siteData.plan))
     .filter(Boolean) as GutsElement[];
   return elementsWithIssue.length ? elementsWithIssue : false;
 };
 
+/**
+ *
+ * @param element
+ * @param plan
+ * @returns false for under limit OR the element back if it has too many routes
+ */
 export const checkElementRouteLimit = (
   element: GutsElement,
   plan: SiteData['plan']

91f5b2a25f9b7ae98b553a7f656c6499e18fdbee
Tyler <tyleraholden@gmail.com>
2022-02-28 14:03:45 -0600

    oops remove import

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,4 @@ export * from './types/User';
 export * from './constants/documentationLinks';
 export * from './constants/PlanLimits';
 
-import './css/colors.scss';
 export * from './utils/checkSiteDataPlanLimits';
